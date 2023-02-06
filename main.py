ce7206099b06ee88b72b71c8f7eb82bfe2b65d6a
Antonis Keremidis <tkeremidis@gmail.com>
2023-02-06 17:22:41 +0200

    Add main function to get samples from wave file

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    import wave
+    import numpy as np
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    with wave.open("myfile.wav", "rb") as wave_file:
+        # Get number of frames
+        num_frames = wave_file.getnframes()
+
+        # Read wave file as string of bytes
+        wave_data = wave_file.readframes(num_frames)
+
+        # Set the datatype to the sample width
+        dtype = np.int16
+
+        # Convert to NumPy array
+        wave_data = np.frombuffer(wave_data, dtype=dtype)
+
+        # Reshape since we have a single channel
+        wave_data = wave_data.reshape((num_frames, 1))
+
