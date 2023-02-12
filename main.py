f787ee1deabdd7ab9600cc7f337aff8356334ba9
Antonis Keremidis <tkeremidis@gmail.com>
2023-02-12 23:31:22 +0200

    Make drawing plots conditional

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     from scipy.io.wavfile import write
     import math
 
+    draw_plots = False
+
     with wave.open("myfile.wav", "rb") as wave_file:
         # Get number of frames
         num_frames = wave_file.getnframes()
@@ -45,28 +47,29 @@ if __name__ == '__main__':
         H = make_mp3_analysisfb(h, M)
 
         # 2-3.Compute Frequency Response of Analysis Filters (Hz)
-        fig1, ax1 = plt.subplots(2, 1, figsize=(15, 15))
+        if draw_plots:
+            fig1, ax1 = plt.subplots(2, 1, figsize=(15, 15))
 
-        for i in range(H.shape[1]):
-            w, h = signal.freqz(H[:, i])
+            for i in range(H.shape[1]):
+                w, h = signal.freqz(H[:, i])
 
-            # f (Hz) = (w * fs)/2π
-            f = w * fs / 2 * np.pi
-            ax1[0].plot(f, 10 * np.log10(abs(h) ** 2))
+                # f (Hz) = (w * fs)/2π
+                f = w * fs / 2 * np.pi
+                ax1[0].plot(f, 10 * np.log10(abs(h) ** 2))
 
-            # bark_f = 13 * arctan(0.00076 * f) + 3.5 * arctan((f / 7500)^2)
-            bark_f = 13 * np.arctan(0.00076 * f) + 3.5 * np.arctan((f / 7500) ** 2)
-            ax1[1].plot(bark_f, 10 * np.log10(abs(h) ** 2))
+                # bark_f = 13 * arctan(0.00076 * f) + 3.5 * arctan((f / 7500)^2)
+                bark_f = 13 * np.arctan(0.00076 * f) + 3.5 * np.arctan((f / 7500) ** 2)
+                ax1[1].plot(bark_f, 10 * np.log10(abs(h) ** 2))
 
-        ax1[0].set_xlabel(r"Frequency (Hz)")
-        ax1[0].set_ylabel(r"$10log_{10}(|H(f)|^2 $")
-        ax1[0].set_title(r"Frequency Response of Analysis Filters (Hz)")
+            ax1[0].set_xlabel(r"Frequency (Hz)")
+            ax1[0].set_ylabel(r"$10log_{10}(|H(f)|^2 $")
+            ax1[0].set_title(r"Frequency Response of Analysis Filters (Hz)")
 
-        ax1[1].set_xlabel(r"Frequency (barks)")
-        ax1[1].set_ylabel(r"$10log_{10}(|H(f)|^2 $")
-        ax1[1].set_title(r"Frequency Response of Analysis Filters (barks)")
+            ax1[1].set_xlabel(r"Frequency (barks)")
+            ax1[1].set_ylabel(r"$10log_{10}(|H(f)|^2 $")
+            ax1[1].set_title(r"Frequency Response of Analysis Filters (barks)")
 
-        plt.show()
+            plt.show()
 
         # 7. Code and decode signal
         N = 36
@@ -74,31 +77,29 @@ if __name__ == '__main__':
         Y_tot, x_hat = codec0(wave_data, H, M, N)
 
         # Plot original and reconstructed wave (amplitude vs time)
-        times = np.linspace(0, num_frames / fs, num=num_frames)
+        if draw_plots:
+            times = np.linspace(0, num_frames / fs, num=num_frames)
 
-        fig2, ax2 = plt.subplots(2, 1, figsize=(15, 15))
-        ax2[0].plot(times, wave_data)
-        ax2[0].set_title('Original wave')
-        ax2[0].set_ylabel('Signal Value')
-        ax2[0].set_xlabel('Time (s)')
-        plt.xlim(0, t_audio)
-        ax2[1].plot(times, x_hat)
-        ax2[1].set_title('Reconstructed wave')
-        ax2[1].set_ylabel('Signal Value')
-        ax2[1].set_xlabel('Time (s)')
-        plt.xlim(0, t_audio)
-        plt.show()
-        breakpoint()
+            fig2, ax2 = plt.subplots(2, 1, figsize=(15, 15))
+            ax2[0].plot(times, wave_data)
+            ax2[0].set_title('Original wave')
+            ax2[0].set_ylabel('Signal Value')
+            ax2[0].set_xlabel('Time (s)')
+            plt.xlim(0, t_audio)
+            ax2[1].plot(times, x_hat)
+            ax2[1].set_title('Reconstructed wave')
+            ax2[1].set_ylabel('Signal Value')
+            ax2[1].set_xlabel('Time (s)')
+            plt.xlim(0, t_audio)
+            plt.show()
 
-        sd.play(wave_data, fs)
-        breakpoint()
-        sd.play(x_hat.astype(np.int16), fs)
-        # write("x_hat.wav", fs, x_hat.astype(np.int16))
+        # Save reconstructed wave
+        write("x_hat.wav", fs, x_hat.astype(np.int16))
 
         # SNR
-
         P_signal = np.mean((np.power(np.squeeze(wave_data), 2 * np.ones(wave_data.shape[0]))))
         P_noise = np.mean(np.power((np.squeeze(wave_data) - x_hat), 2 * np.ones(wave_data.shape[0])))
         SNR = P_signal / P_noise
         SNR_dB = 10 * np.log10(SNR)
+
         breakpoint()
