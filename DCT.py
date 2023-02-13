da33d109f910554479c05572ea987c83ee3dd46a
cvkoutsi <ckoutsi26@gmail.com>
2023-02-13 18:59:20 +0200

    smol change to dct

--- DCT.py
+++ DCT.py
@@ -1,6 +1,6 @@
 from scipy.fftpack import dct,idct
 def frameDCT(Y):
-    return dct(Y).reshape(-1,1)
+    return dct(Y,norm = 'ortho').reshape(-1,1)
 
 def iframeDCT(c,M,N):
-    return idct(c).reshape(M,N)
+    return idct(c,norm = 'ortho').reshape(N,M)
