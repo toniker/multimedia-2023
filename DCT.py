f8792ed2574afca3333f2d6e3de43ae4d418429b
Antonis Keremidis <tkeremidis@gmail.com>
2023-02-14 13:35:23 +0200

    Add DCTpower function

--- DCT.py
+++ DCT.py
@@ -1,6 +1,15 @@
-from scipy.fftpack import dct,idct
-def frameDCT(Y):
-    return dct(Y,norm = 'ortho').reshape(-1,1)
+import numpy as np
+from scipy.fftpack import dct, idct
 
-def iframeDCT(c,M,N):
-    return idct(c,norm = 'ortho').reshape(N,M)
+
+def frameDCT(Y):
+    return dct(Y, norm='ortho').reshape(-1, 1)
+
+
+def iframeDCT(c, M, N):
+    return idct(c, norm='ortho').reshape(N, M)
+
+
+def DCTpower(c):
+    to_log = lambda x: 10 * np.log10(abs(x)**2)
+    return np.vectorize(to_log)(c)
