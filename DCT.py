d27e1e7fb456faa2b75e27abf5af8522495351a7
Antonis Keremidis <tkeremidis@gmail.com>
2023-02-18 19:26:48 +0200

    Replace DCT functions

--- DCT.py
+++ DCT.py
@@ -1,11 +1,19 @@
 import numpy as np
-from scipy.fftpack import dct,idct
-def frameDCT(Y):
-    return dct(Y,norm = 'ortho').reshape(-1,1)
+from scipy.fftpack import dct, idct
 
-def iframeDCT(c,M,N):
-    return idct(c,norm = 'ortho').reshape(N,M)
+
+def frameDCT(Y):
+    dct_result = np.array(())
+    for row in Y:
+        dct_result = np.append(dct_result, dct(row, norm='ortho', overwrite_x=True))
+    return np.array(dct_result).reshape(-1, 1)
+
+def iframeDCT(c, M, N):
+    idct_result = np.array(())
+    for row in c:
+        idct_result = np.append(idct_result, idct(row, norm='ortho', overwrite_x=True))
+    return np.array(idct_result).reshape(N, M)
+
 
 def DCTpower(c):
-    return 10*np.log10(c ** 2)
-
+    return 10 * np.log10(c ** 2)
